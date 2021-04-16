9426b95122ab6f34650af096457a761fcdf00864
nathan-louie <nathanhlouie@gmail.com>
2021-04-16 16:34:02 -0400

    Add template home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,32 @@
 <template>
   <div id="home" class="wrapper">
-    <div class="static-container"></div>
+    <div class="static-container">
+      <h1 class="title">
+        Nathan Louie
+      </h1>
+      <div class="std">
+        <subtitle class="-big">
+          I'm a high-school developer making projects I find fun!
+        </subtitle>
+        <ul class="home-list">
+          <li>
+            <h2>My ongoing projects:</h2>
+          </li>
+          <li>
+            - Fischer Random chess website ♛
+          </li>
+          <li>
+            - Teach Assist mark tracker 🏫
+          </li>
+          <li>
+            - Competitive programming 💻
+          </li>
+        </ul>
+        <h2 class="-end">
+          Feel free to look around!
+        </h2>
+      </div>
+    </div>
   </div>
 </template>
 
